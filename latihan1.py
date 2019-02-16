0d6915f11f7f47a54a4b51b7df9edb50abf709b5
namakamu123 <44119437+namakamu123@users.noreply.github.com>
2019-02-16 21:39:17 +0700

    mencetak nilai secara acak yg lebih kecil dari 0.5

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+import random
+i = 1
+n = int (input ("masukan nilai N :"))
+
+while i <= n :
+    a = random.uniform(0.0,0.5)
+    print ("data ke -",i, "adalah =>",a)
+    i += 1
+print("selesai")
+
